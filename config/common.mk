cc6c55cd283eccbc1e722ea64d6f42cb349b3735
LineageOS <lineageos.org>
2018-08-13 04:16:05 +0000

    add backup tool from lineage

--- config/common.mk
+++ config/common.mk
@@ -13,3 +13,18 @@ include vendor/lean/config/lean_props.mk
 
  # Packages
 include vendor/lean/config/packages.mk
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/lean/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/lean/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/lean/prebuilt/common/bin/50-lean.sh:system/addon.d/50-lean.sh \
+    vendor/lean/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/lean/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/lean/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/lean/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
+
