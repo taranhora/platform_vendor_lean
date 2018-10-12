bed76f2cb69177643f5944d1f323d4dc288e2a20
premaca <premaca@gmail.com>
2018-10-12 18:06:52 +0000

    Added and updated Some files

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,2 @@
-for combo in $(curl -s  https://raw.githubusercontent.com/LeanOS-Project/platform_vendor_lean/lean-9.x/lean.devices | sed -e 's/#.*$//' | awk '{printf "lean_%s-%s\n", $1, $2}')
-do
-    add_lunch_combo $combo
-done
+. "$(gettop)/vendor/lean/build/envsetup.sh"
+ lean_device_combos
