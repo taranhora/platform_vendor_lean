274612c2e5e459227608535bd672f0820c67c466
Jyotiraditya <jyotiraditya182@gmail.com>
2018-10-26 19:52:59 +0000

    version: Added Lean Specific Beta , Alpha and Official tag
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -3,8 +3,34 @@ PRODUCT_VERSION_MAJOR = 9.0
 PRODUCT_VERSION_MAINTENANCE = LoL
 LEAN_POSTFIX := $(shell date +"%Y%m%d")
 LEAN_BUILD_EXTRA := By-Team-Lean
+
+# Default Unofficial Tag
 ifndef LEAN_BUILD_TYPE
     LEAN_BUILD_TYPE := UNOFFICIAL
+endif
+
+# Beta Tag
+ifeq ($(LEAN_BETA),true)
+    LEAN_BUILD_TYPE := BETA
+endif
+
+# Alpha Tag
+ifeq ($(LEAN_ALPHA),true)
+   LEAN_BUILD_TYPE := ALPHA
+endif
+
+# Specific Official Tag
+CURRENT_DEVICE=$(shell echo "$(TARGET_PRODUCT)" | cut -d'_' -f 2,3)
+
+ifeq ($(LEAN_OFFICIAL),true)
+   LIST = $(shell curl -s https://raw.githubusercontent.com/LeanOS-Project/platform_vendor_lean/lean-9.x/lean.devices)
+   FOUND_DEVICE = $(filter $(CURRENT_DEVICE), $(LIST))
+   ifeq ($(FOUND_DEVICE),$(CURRENT_DEVICE))
+      IS_OFFICIAL=true
+      LEAN_BUILD_TYPE := OFFICIAL
+   else
+      LEAN_BUILD_TYPE := UNOFFICIAL
+   endif
 endif
 
 # Set all versions
